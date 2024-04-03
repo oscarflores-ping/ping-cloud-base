225e617c5ac6c53fe61f0c667b1ce481946688f2
oscar.flores <oscarflores@pingidentity.com>
2024-04-03 14:24:46 -0600

    v1.19 resync

--- code-gen/push-cluster-state.sh
+++ code-gen/push-cluster-state.sh
@@ -100,6 +100,24 @@ is_all_apps() {
 }
 
 ########################################################################################################################
+# We need some way to identify that we are indeed within a cluster-state-repo or profile-repo before making irreversible
+# changes. Therefore, check that either a 'profiles', 'values-files', or 'k8s-configs' (for monorepo backwards
+# compatibility) directory exists, or that the directory is empty (besides hidden/dotfiles) in the case of a brand-new
+# CSR/PR empty cloned repo
+########################################################################################################################
+dir_sanity_check() {
+  echo "Checking if in a CSR or PR..."
+  if [[ -d "profiles" || \
+        -d "values-files" || \
+        -d "k8s-configs" || \
+        $(find . -mindepth 1 -not -path "./.*" | wc -l | tr -d '[:space:]') == 0 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+########################################################################################################################
 # Switch back to the previous branch and delete the staging branch.
 ########################################################################################################################
 finalize() {
@@ -109,6 +127,13 @@ finalize() {
 }
 
 ### Script start ###
+
+if [[ "${IS_BELUGA_ENV}" == "true" ]]; then
+  if ! dir_sanity_check; then
+    echo "Directory sanity check failed, please check that you are running push-cluster-state.sh from a CSR or PR!!"
+    exit 1
+  fi
+fi
 
 # If profile repo and secondary region, early-out. The profiles will be exactly identical for all regions and should
 # already have been seeded when this script was run on primary region.
@@ -221,6 +246,7 @@ for ENV_OR_BRANCH in ${SUPPORTED_ENVIRONMENT_TYPES}; do
       # Attempt to create the branch from its default CDE or CHUB branch name.
       echo "Branch ${GIT_BRANCH} does not exist locally"
 
+      # Check if DEFAULT_CDE_BRANCH branch exists locally
       if git rev-parse --verify "${DEFAULT_CDE_BRANCH}" &> /dev/null; then
         # This block will be executed only during updates. At the time, we want to capture history
         # of all changes in the old CDE or customer-hub branches onto the new ones that are created.
@@ -233,7 +259,15 @@ for ENV_OR_BRANCH in ${SUPPORTED_ENVIRONMENT_TYPES}; do
         # exists, onto the orphan branch in the following block.
         echo "Default branch ${DEFAULT_CDE_BRANCH} does not exist for branch ${GIT_BRANCH}"
         echo "Creating ${GIT_BRANCH} as an orphan branch"
+        # Use existing git checkout --orphan as git switch --orphan is newer and may not be supported everywhere
         git checkout --orphan "${GIT_BRANCH}"
+        # Remove all files to prevent the previous branch's files from being brought into the new orphaned branch.
+        # This is critical for the Versent multi-region generation process as they generate the secondary region in
+        # isolation - if the Versent process fails in the secondary region while still in isolation, keeping the files
+        # pulled from the secondary CSR from the previous branch (master) can lead to unexpected behavior for non-master
+        # branches
+        echo "Attempting to delete contents of previous branch before orphan branch was created..."
+        git rm -rf . || echo "Previous git branch was already empty"
       fi
     fi
   fi
