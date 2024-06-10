cd96acd02e667a0745928cb958742f00f4b15468
oscar.flores <oscarflores@pingidentity.com>
2024-06-10 11:07:49 -0600

    update to match pcb

--- k8s-configs/cluster-tools/base/karpenter/base/update-karpenter.sh
+++ k8s-configs/cluster-tools/base/karpenter/base/update-karpenter.sh
@@ -15,10 +15,10 @@ else
     exit 1
 fi
 
-# helm template karpenter oci://public.ecr.aws/karpenter/karpenter --version ${KARPENTER_VERSION} --namespace kube-system \
-#     --set settings.aws.defaultInstanceProfile=KarpenterInstanceProfile \
-#     --version ${KARPENTER_VERSION} > karpenter.yaml
-# exit
+helm template karpenter oci://public.ecr.aws/karpenter/karpenter --version ${KARPENTER_VERSION} --namespace kube-system \
+    --set settings.aws.defaultInstanceProfile=KarpenterInstanceProfile \
+    --version ${KARPENTER_VERSION} > karpenter.yaml
+
 wget https://raw.githubusercontent.com/aws/karpenter-provider-aws/${KARPENTER_VERSION}/pkg/apis/crds/karpenter.k8s.aws_ec2nodeclasses.yaml
 wget https://raw.githubusercontent.com/aws/karpenter-provider-aws/${KARPENTER_VERSION}/pkg/apis/crds/karpenter.sh_nodeclaims.yaml
 wget https://raw.githubusercontent.com/aws/karpenter-provider-aws/${KARPENTER_VERSION}/pkg/apis/crds/karpenter.sh_nodepools.yaml
