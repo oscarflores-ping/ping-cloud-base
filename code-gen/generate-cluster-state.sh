734a8ebb9b2271e62d62b40a2433976b187690e0
oscar.flores <oscarflores@pingidentity.com>
2025-03-14 09:29:20 -0600

    pulling latest

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -2,7 +2,7 @@
 
 # If VERBOSE is true, then output line-by-line execution
 "${VERBOSE:-false}" && set -x
-"${EXIT_ON_FAILURE:-false}" && set -e
+"${EXIT_ON_FAILURE:-true}" && set -e
 
 ########################################################################################################################
 #
@@ -1536,7 +1536,7 @@ for ENV_OR_BRANCH in ${SUPPORTED_ENVIRONMENT_TYPES}; do
   echo "=====> Done creating environment '${ENV}'"
 )
 subshell_exit_code=$?
-if [[ $subshell_exit_code -ne 0 ]]; then
+if [[ $subshell_exit_code -ne 0 && "$EXIT_ON_FAILURE" == "true" ]]; then
     echo "Environment '${ENV}' creation failed with exit code ${subshell_exit_code}"
     exit $subshell_exit_code
 fi
