4c8bec54d42fd590cf95e4085c333f7f9d0d1f76
oscarflores <oscarflores@pingidentity.com>
2026-04-21 16:07:50 -0600

    updating install script

--- k8s-configs/cluster-tools/base/git-ops/argo/install-custom-tools.sh
+++ k8s-configs/cluster-tools/base/git-ops/argo/install-custom-tools.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ex
 
 ### git-remote-codecommit ###
-pip install --upgrade pip
-pip3 install git-remote-codecommit --no-warn-script-location
+#pip install --upgrade pip
+#pip3 install git-remote-codecommit --no-warn-script-location
 
 cp /usr/local/bin/git-remote-codecommit /tools
 cp /usr/local/lib/python3.9/site-packages/git_remote_codecommit/__init__.py /tools
@@ -12,10 +12,10 @@ sed -i 's|/usr/local/bin/python|/usr/bin/python3|' /tools/git-remote-codecommit
 chmod a+x /tools/git-remote-codecommit
 
 ### envsubst and wget ###
-apt-get update
-apt-get -y install gettext-base wget
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+#apt-get update
+#apt-get -y install gettext-base wget
+#apt-get clean
+#rm -rf /var/lib/apt/lists/*
 cp /usr/bin/envsubst /tools
 
 ### Install specific Kustomize versions - one for backwards compatibility as well as a new version ###
